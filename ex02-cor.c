651227d38562ccf33feeb9fb3d042e2ca14cf3ef
Bruno Pires <62072617+psbruno@users.noreply.github.com>
2020-12-05 14:57:14 -0300

    Create ex02-cor.c

---
+++ ex02-cor.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <GL/glut.h>
+#include <math.h>
+# define PI 3.141592654
+int n = 50;
+float ang = 50;
+float x,y;
+float r=0,g=0,b=1;
+
+
+void primeiro(){
+   printf("%f %f\n", x/1000,y/1000);
+    int i;
+    float angulo=0;
+
+    glClearColor(0, 0, 0, 0); //Preto
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glPointSize(50);//aumentei o tamanho para conseguir ver
+    glColor3f(r,g,b);
+    glBegin(GL_POINTS);
+        glVertex2f((4*x/1000-1), (1-4*y/1000));
+
+
+        //para o pontos circulares:
+/*
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_POLYGON);
+        glColor3f(0.0,0.0, 1.0);
+        for(i=0;i<500;i++){
+            angulo = 2* PI * i / 50;
+            //if(i==1 || i==100)printf("__%f %f\n",0.05*cos(angulo), 0.05*sin(angulo) );
+            glVertex2f(0.05*cos(angulo)+(4*x/1000-1),0.05*sin(angulo)+(1-4*y/1000));
+            //glVertex2f(0.05*cos(angulo)+ x/1000+0.3,0.05*sin(angulo)+y/1000-0.3);
+        }
+*/
+        //para pontos circulares
+
+    glEnd();
+    glFlush();
+}
+void desenha(){
+    printf("%f %f\n", x/1000,y/1000);
+    int i;
+    float angulo=0;
+
+    glClearColor(0, 0, 0, 0); //Preto
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glPointSize(5);
+    glColor3f(r,g,b);
+
+    glBegin(GL_POINTS);
+        glVertex2f((4*x/1000-1), (1-4*y/1000));
+
+
+        //para pontos circulares:
+/*
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_POLYGON);
+        glColor3f(0.0,0.0, 1.0);
+        for(i=0;i<500;i++){
+            angulo = 2* PI * i / 50;
+            //if(i==1 || i==100)printf("__%f %f\n",0.05*cos(angulo), 0.05*sin(angulo) );
+            glVertex2f(0.05*cos(angulo)+(4*x/1000-1),0.05*sin(angulo)+(1-4*y/1000));
+            //glVertex2f(0.05*cos(angulo)+ x/1000+0.3,0.05*sin(angulo)+y/1000-0.3);
+        }
+*/
+        //para pontos circulares
+
+    glEnd();
+    glFlush();
+}
+void Teclado(unsigned char key, int x, int y){
+    switch (key){
+        case '0' :
+            r=0.243;
+            g=0.5478;
+            b=0.789;
+            break;
+
+
+        case '1' :
+            r=0;
+            g=0.8;
+            b=0;
+        break;
+
+
+        case '2' :
+            r=1;
+            g=0.27;
+            b=0.7;
+        break;
+
+        case '3' :
+            r=0.8;
+            g=0.2;
+            b=1;
+        break;
+
+        case '4' :
+            r=1.7;
+            g=0.4;
+            b=0.2;
+        break;
+
+        case '5' :
+            r=0.4;
+            g=1;
+            b=1;
+        break;
+
+        case '6' :
+            r=1;
+            g=1;
+            b=1;
+        break;
+
+        case '7' :
+            r=1;
+            g=1;
+            b=0;
+        break;
+
+        case '8' :
+            r=0.718;
+            g=0.61;
+            b=0.748;
+        break;
+
+        case '9' :
+            r=0;
+            g=1;
+            b=0.6;
+        break;
+    }
+    glutPostRedisplay() ;
+
+}
+void MouseInt (int botao, int estado, int xl, int yl){
+
+    switch(botao){
+       case GLUT_LEFT_BUTTON:
+          if(estado == GLUT_DOWN)
+              glutDisplayFunc (desenha);
+              x=xl;
+              y=yl;
+
+          break;
+    }
+    glutPostRedisplay();
+}
+
+
+int main( int argc, char *argv[ ] ){
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(500,100);
+    glutInitWindowSize(500,500);
+    glutInit(&argc, argv);
+    glutCreateWindow("Circulo em torno do click");
+ //   glutDisplayFunc(setajanela);
+    glutDisplayFunc (primeiro);
+    glutMouseFunc(MouseInt);
+    glutKeyboardFunc(Teclado);
+    glutMainLoop ();
+    return 0;
+}
