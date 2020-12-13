13c3f056584a69166403c52231ba22569bd1700d
Bruno Pires <62072617+psbruno@users.noreply.github.com>
2020-12-13 17:49:13 -0300

    Update ex01-Flor.c

--- s3/ex01-Flor.c
+++ s3/ex01-Flor.c
@@ -5,124 +5,87 @@
 #include <stdlib.h>
 #include <math.h>
 # define PI 3.141592654
-float tx = 0.0;
+float tx =0.0;
 float ty = 0.0;
 int n = 10;
 float ang = 0;
 int lado = 1;
+float r=0.0,g=0.0,b=0.0;
 
 void display_function()
 {
+    glClearColor(1, 1, 1, 0);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Considerando um quadrado cujos vértices possuem as coordenadas
-    // x1 = -0.25 y1 = 0.25
-    // x2 = 0.25  y2 = 0.25
-    // x3 = 0.25  y3 = -0.25
-    // x4 = -0.25 y4 = -0.25
-
-    //Lembrando que para a translação por (tx,ty)
-    // x' = x + tx
-    // y' = y + ty
-    int i;
-    float angulo;
-    glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POLYGON);
-     glColor3f(0.0,1.0, 0.0);
-         for(i=0;i<180;i++){
-            glVertex2f(0.4*cos(ang)+0.5,0.4*sin(ang));
-            ang = ang+(2*M_PI)/n;
-        }
+    glColor3f(0.75,0.75,0.75);
 
-    glEnd();
-    glBegin(GL_POLYGON);
-     glColor3f(0.0,1.0, 0.0);
-         for(i=0;i<180;i++){
-            glVertex2f(0.4*cos(ang),0.4*sin(ang)-0.5);
-            ang = ang+(2*M_PI)/n;
-        }
-
-    glEnd();
-      glBegin(GL_POLYGON);
-     glColor3f(0.0,1.0, 0.0);
-         for(i=0;i<180;i++){
-            glVertex2f(0.4*cos(ang),0.4*sin(ang)+0.5);
-            ang = ang+(2*M_PI)/n;
-        }
-
-    glEnd();
-      glBegin(GL_POLYGON);
-     glColor3f(0.0,1.0, 0.0);
-         for(i=0;i<180;i++){
-            glVertex2f(0.4*cos(ang)-0.5,0.4*sin(ang));
-            ang = ang+(2*M_PI)/n;
-        }
-
-    glEnd();
-    glBegin(GL_POLYGON);
-     glColor3f(1.0,1.0, 1.0);
-         for(i=0;i<180;i++){
-            glVertex2f(0.4*cos(ang)+0.35,0.4*sin(ang)+0.35);
-            ang = ang+(2*M_PI)/n;
-        }
-
-    glEnd();
-
-     glBegin(GL_POLYGON);
-     glColor3f(1.0,0.0, 0.0);
-         for(i=0;i<180;i++){
-            glVertex2f(0.4*cos(ang)+0.35,0.4*sin(ang)-0.35);
-            ang = ang+(2*M_PI)/n;
-        }
-    glEnd();
-     glBegin(GL_POLYGON);
-     glColor3f(1.0,0.0, 0.0);
-         for(i=0;i<180;i++){
-            glVertex2f(0.4*cos(ang)-0.35,0.4*sin(ang)+0.35);
-            ang = ang+(2*M_PI)/n;
-        }
-    glEnd();
-     glBegin(GL_POLYGON);
-     glColor3f(1.0,1.0, 1.0);
-         for(i=0;i<180;i++){
-            glVertex2f(0.4*cos(ang)-0.35,0.4*sin(ang)-0.35);
-            ang = ang+(2*M_PI)/n;
-        }
+        glVertex2f(-0.4+tx,0.0);
+        glVertex2f(0.4+tx,0.0);
+        glVertex2f(0.4+tx,-0.2);
+        glVertex2f(-0.4+tx,-0.2);
     glEnd();
 
     glBegin(GL_POLYGON);
-     glColor3f(1.0,1.0, 0.0);
-        for(i=0;i<180;i++){
-            glVertex2f(0.25*cos(ang),0.25*sin(ang));
+    glColor3f(0.0,0.78,1.0);
+        glVertex2f(-0.3+tx,0.0);
+        glVertex2f(-0.2+tx,0.0);
+        glVertex2f(-0.2+tx,0.2);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+     glColor3f(0.0,0.78,1.0);
+        glVertex2f(-0.2+tx,0.2);
+        glVertex2f(0.2+tx,0.2);
+        glVertex2f(0.2+tx,0.0);
+        glVertex2f(-0.2+tx,0.0);
+    glEnd();
+    glBegin(GL_POLYGON);
+        glColor3f(0.0,0.78,1.0);
+        glVertex2f(0.2+tx,0.2);
+        glVertex2f(0.3+tx,0.0);
+        glVertex2f(0.2+tx,0.0);
+    glEnd();
+    //glRotatef(180.0, 0.0f, 0.0f, 1.0f);
+    glBegin(GL_POLYGON);
+  //  glRotatef(180.0, 1.0f, 1.0f, 1.0f);
+
+     glColor3f(r,g, b);
+    for(int i=0;i<180;i++){
+            glVertex2f(0.08*cos(ang)-0.3+tx,0.1*sin(ang)-0.17);
             ang = ang+(2*M_PI)/n;
         }
     glEnd();
 
 
-
-    glRotatef(20.0f*lado, 0.0f, 0.0f, 1.0f);
+    glBegin(GL_POLYGON);
+     glColor3f(r,g, b);
+         for(int i=0;i<180;i++){
+            glVertex2f(0.08*cos(ang)+0.3+tx,0.1*sin(ang)-0.17);
+            ang = ang+(2*M_PI)/n;
+        }
+    glEnd();
     glFlush();
 }
 
 void tecladoEspecial(int tecla, int x, int y){
     switch (tecla) {
         case GLUT_KEY_RIGHT:
-        case GLUT_KEY_UP:
-            lado=-1;
+            tx+=0.05;
             break;
         case GLUT_KEY_LEFT:
-        case GLUT_KEY_DOWN:
+            tx-=0.05;
+            break;
+        case GLUT_KEY_UP:
             lado=1;
+            break;
+        case GLUT_KEY_DOWN:
+            lado=-1;
             break;
         default:
             break;
     }
     glutPostRedisplay();
-}
-
-void init(){
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glColor3f(1.0,1.0,1.0);
 }
 
 int main(int argc, char **argv)
