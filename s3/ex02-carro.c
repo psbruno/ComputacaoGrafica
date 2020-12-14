400caed5857d338b09e13ab718ab15d2b625ed1a
Bruno Pires <62072617+psbruno@users.noreply.github.com>
2020-12-14 16:16:47 -0300

    Update ex02-carro.c

--- s3/ex02-carro.c
+++ s3/ex02-carro.c
@@ -9,71 +9,89 @@ float tx =0.0;
 float ty = 0.0;
 int n = 10;
 float ang = 0;
-int lado = 1;
+float theta = 0.0;
 float r=0.0,g=0.0,b=0.0;
-void roda(){
-    glClear(GL_COLOR_BUFFER_BIT);
+void rodaT(){
+    glTranslatef(-0.25,-0.17,0.0);
+    glRotatef(theta,-0.3,-0.17,1.0);
+    glBegin(GL_POLYGON);
+     glColor3f(r,g, b);
+         for(int i=0;i<10;i++){
+            glVertex2f(0.1*cos(ang),0.1*sin(ang));
+            ang = ang+(2*M_PI)/10;
+        }
+
+    glEnd();
 
 
 }
-void display_function()
-{
-    glClearColor(1, 1, 1, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
+void rodaD(){
+    glTranslatef(+0.25,-0.17,0.0);
+    glRotatef(theta,-0.3,-0.17,1.0);
     glBegin(GL_POLYGON);
+     glColor3f(r,g, b);
+         for(int i=0;i<10;i++){
+            glVertex2f(0.1*cos(ang),0.1*sin(ang));
+            ang = ang+(2*M_PI)/10;
+        }
+
+    glEnd();
+
+
+}
+void carro(){
+    glBegin(GL_POLYGON);
+    //corpo do carro
     glColor3f(0.75,0.75,0.75);
 
-        glVertex2f(-0.4+tx,0.0);
-        glVertex2f(0.4+tx,0.0);
-        glVertex2f(0.4+tx,-0.2);
-        glVertex2f(-0.4+tx,-0.2);
+        glVertex2f(-0.4,0.0);
+        glVertex2f(0.4,0.0);
+        glVertex2f(0.4,-0.2);
+        glVertex2f(-0.4,-0.2);
     glEnd();
+
+}
+void vidro(){
 
     glBegin(GL_POLYGON);
     glColor3f(0.0,0.78,1.0);
-        glVertex2f(-0.3+tx,0.0);
-        glVertex2f(-0.2+tx,0.0);
-        glVertex2f(-0.2+tx,0.2);
+        glVertex2f(-0.3,0.0);
+        glVertex2f(-0.2,0.0);
+        glVertex2f(-0.2,0.2);
     glEnd();
 
     glBegin(GL_POLYGON);
      glColor3f(0.0,0.78,1.0);
-        glVertex2f(-0.2+tx,0.2);
-        glVertex2f(0.2+tx,0.2);
-        glVertex2f(0.2+tx,0.0);
-        glVertex2f(-0.2+tx,0.0);
+        glVertex2f(-0.2,0.2);
+        glVertex2f(0.2,0.2);
+        glVertex2f(0.2,0.0);
+        glVertex2f(-0.2,0.0);
     glEnd();
     glBegin(GL_POLYGON);
     glColor3f(0.0,0.78,1.0);
-        glVertex2f(0.2+tx,0.2);
-          glVertex2f(0.3+tx,0.0);
-          glVertex2f(0.2+tx,0.0);
+        glVertex2f(0.2,0.2);
+          glVertex2f(0.3,0.0);
+          glVertex2f(0.2,0.0);
     glEnd();
+    //vidros
+}
+void display_function()
+{
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glClearColor(1, 1, 1, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    glBegin(GL_POLYGON);
-  //  glRotatef(180.0, 1.0f, 1.0f, 1.0f);
+    glTranslatef(tx,0.0,0.0);
+    carro();
+    vidro();
+    glPushMatrix();
+        rodaT();
+    glPopMatrix();
 
-     glColor3f(r,g, b);
-         for(int i=0;i<180;i++){
-            glVertex2f(0.08*cos(ang)-0.3+tx,0.1*sin(ang)-0.17);
-            ang = ang+(2*M_PI)/n;
-        }
-
-    glRotatef(180.0, 1.0f, 1.0f, 1.0f);
-    glEnd();
-    glBegin(GL_POLYGON);
-  //  glRotatef(180.0, 1.0f, 1.0f, 1.0f);
-
-     glColor3f(r,g, b);
-         for(int i=0;i<180;i++){
-            glVertex2f(0.08*cos(ang)+0.3+tx,0.1*sin(ang)-0.17);
-            ang = ang+(2*M_PI)/n;
-        }
-
-    glRotatef(180.0, 1.0f, 1.0f, 1.0f);
-    glEnd();
-
+    glPushMatrix();
+        rodaD();
+    glPopMatrix();
     glFlush();
 }
 
@@ -81,22 +99,25 @@ void tecladoEspecial(int tecla, int x, int y){
     switch (tecla) {
         case GLUT_KEY_RIGHT:
             tx+=0.05;
+            theta+=5;
             break;
         case GLUT_KEY_LEFT:
             tx-=0.05;
+            theta-=5;
             break;
         case GLUT_KEY_UP:
-            r+=0.05;
+             r+=0.05;
             if(g<0.3)
                 g+=0.025;
-                lado=1;
+            theta+=5;
             break;
         case GLUT_KEY_DOWN:
-            r+=0.05;
+             r+=0.05;
             if(g<0.3)
                 g+=0.025;
-            lado=-1;
+            theta-=5;
             break;
+
         default:
             break;
     }
